37f2dc011fe35e486b0ce2d612b93309849beea8
temisAP <74715759+temisAP@users.noreply.github.com>
2021-06-07 01:13:10 +0200

    Update aiart.py

--- src/aiart.py
+++ src/aiart.py
@@ -7,8 +7,8 @@ from tensorflow import keras
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-content_path = './images/city.jpg'
-style_path = './images/stary_night'
+content_path = '../images/city.jpg'
+style_path = '../images/stary_night.jpg'
 
 def load_img(path_to_img):
   max_dim = 512
@@ -16,17 +16,17 @@ def load_img(path_to_img):
   long = max(img.size)
   scale = max_dim/long
   img = img.resize((round(img.size[0]*scale), round(img.size[1]*scale)), Image.ANTIALIAS)
-  
+
   img = kp_image.img_to_array(img)
-  
-  # We need to broadcast the image array such that it has a batch dimension 
+
+  # We need to broadcast the image array such that it has a batch dimension
   img = np.expand_dims(img, axis=0)
   return img
 
 def imshow(img, title=None):
   # Remove the batch dimension
   out = np.squeeze(img, axis=0)
-  # Normalize for display 
+  # Normalize for display
   out = out.astype('uint8')
   plt.imshow(out)
   if title is not None:
@@ -41,7 +41,7 @@ def deprocess_img(processed_img):
                              "dimension [1, height, width, channel] or [height, width, channel]")
   if len(x.shape) != 3:
     raise ValueError("Invalid input to deprocessing image")
-  
+
   # perform the inverse of the preprocessing step
   x[:, :, 0] += 103.939
   x[:, :, 1] += 116.779
@@ -68,13 +68,13 @@ plt.subplot(1, 2, 2)
 imshow(style, 'Style Image')
 plt.show()
 
-content_layers = ['block5_conv2'] 
+content_layers = ['block5_conv2']
 
 # Style layer we are interested in
 style_layers = ['block1_conv1',
                 'block2_conv1',
-                'block3_conv1', 
-                'block4_conv1', 
+                'block3_conv1',
+                'block4_conv1',
 		        'block5_conv1']
 
 num_content_layers = len(content_layers)
@@ -94,7 +94,7 @@ def get_content_loss(base_content, target):
   return tf.reduce_mean(tf.square(base_content - target))
 
 def gram_matrix(input_tensor):
-    # We make the image channels first 
+    # We make the image channels first
     channels = int(input_tensor.shape[-1])
     a = tf.reshape(input_tensor, [-1, channels])
     n = tf.shape(a)[0]
@@ -105,20 +105,20 @@ def get_style_loss(base_style, gram_target):
     """Expects two images of dimension h, w, c"""
     height, width, channels = base_style.get_shape().as_list()
     gram_style = gram_matrix(base_style)
-    
+
     return tf.reduce_mean(tf.square(gram_style - gram_target))
 
 def get_feature_representations(model, content_path, style_path):
-    # Load our images in 
+    # Load our images in
     content_image = load_and_process_img(content_path)
     style_image = load_and_process_img(style_path)
-    
+
     # batch compute content and style features
     style_outputs = model(style_image)
     content_outputs = model(content_image)
-    
-    
-    # Get the style and content feature representations from our model  
+
+
+    # Get the style and content feature representations from our model
     style_features = [style_layer[0] for style_layer in style_outputs[:num_style_layers]]
     content_features = [content_layer[0] for content_layer in content_outputs[num_style_layers:]]
     return style_features, content_features
@@ -126,13 +126,13 @@ def get_feature_representations(model, content_path, style_path):
 def compute_loss(model, loss_weights, init_image, gram_style_features, content_features):
 
     style_weight, content_weight = loss_weights
-    
+
 
     model_outputs = model(init_image)
-    
+
     style_output_features = model_outputs[:num_style_layers]
     content_output_features = model_outputs[num_style_layers:]
-    
+
     style_score = 0
     content_score = 0
 
@@ -140,21 +140,21 @@ def compute_loss(model, loss_weights, init_image, gram_style_features, content_f
     weight_per_style_layer = 1.0 / float(num_style_layers)
     for target_style, comb_style in zip(gram_style_features, style_output_features):
         style_score += weight_per_style_layer * get_style_loss(comb_style[0], target_style)
-        
+
 
     weight_per_content_layer = 1.0 / float(num_content_layers)
     for target_content, comb_content in zip(content_features, content_output_features):
         content_score += weight_per_content_layer* get_content_loss(comb_content[0], target_content)
-    
+
     style_score *= style_weight
     content_score *= content_weight
 
 
-    loss = style_score + content_score 
+    loss = style_score + content_score
     return loss, style_score, content_score
 
 def compute_grads(cfg):
-  with tf.GradientTape() as tape: 
+  with tf.GradientTape() as tape:
     all_loss = compute_loss(**cfg)
   # Compute gradients wrt input image
   total_loss = all_loss[0]
@@ -162,34 +162,34 @@ def compute_grads(cfg):
 
 
 import time
-def run_style_transfer(content_path, 
+def run_style_transfer(content_path,
                        style_path,
                        num_iterations=1000,
-                       content_weight=1e3, 
-                       style_weight=1e-2): 
+                       content_weight=1e3,
+                       style_weight=1e-2):
   # We don't need to (or want to) train any layers of our model, so we set their
-  # trainable to false. 
-  model = get_model() 
+  # trainable to false.
+  model = get_model()
   for layer in model.layers:
     layer.trainable = False
-  
-  # Get the style and content feature representations (from our specified intermediate layers) 
+
+  # Get the style and content feature representations (from our specified intermediate layers)
   style_features, content_features = get_feature_representations(model, content_path, style_path)
   gram_style_features = [gram_matrix(style_feature) for style_feature in style_features]
-  
+
   # Set initial image
   init_image = load_and_process_img(content_path)
   init_image = tf.Variable(init_image, dtype=tf.float32)
   # Create our optimizer
   opt = keras.optimizers.Adam(learning_rate=5)
 
-  # For displaying intermediate images 
+  # For displaying intermediate images
   iter_count = 1
-  
+
   # Store our best result
   best_loss, best_img = float('inf'), None
-  
-  # Create a nice config 
+
+  # Create a nice config
   loss_weights = (style_weight, content_weight)
   cfg = {
       'model': model,
@@ -198,18 +198,18 @@ def run_style_transfer(content_path,
       'gram_style_features': gram_style_features,
       'content_features': content_features
   }
-    
+
   # For displaying
   num_rows = 2
   num_cols = 5
   display_interval = num_iterations/(num_rows*num_cols)
   start_time = time.time()
   global_start = time.time()
-  
+
   norm_means = np.array([103.939, 116.779, 123.68])
   min_vals = -norm_means
-  max_vals = 255 - norm_means   
-  
+  max_vals = 255 - norm_means
+
   imgs = []
   for i in range(num_iterations):
     print(i)
@@ -218,22 +218,22 @@ def run_style_transfer(content_path,
     opt.apply_gradients([(grads, init_image)])
     clipped = tf.clip_by_value(init_image, min_vals, max_vals)
     init_image.assign(clipped)
-    end_time = time.time() 
-    
+    end_time = time.time()
+
     if loss < best_loss:
-      # Update best loss and best image from total loss. 
+      # Update best loss and best image from total loss.
       best_loss = loss
       best_img = deprocess_img(init_image.numpy())
 
     if i % display_interval== 0:
       start_time = time.time()
-      
+
       # Use the .numpy() method to get the concrete numpy array
       plot_img = init_image.numpy()
       plot_img = deprocess_img(plot_img)
       imgs.append(plot_img)
-      print('Iteration: {}'.format(i))        
-      print('Total loss: {:.4e}, ' 
+      print('Iteration: {}'.format(i))
+      print('Total loss: {:.4e}, '
             'style loss: {:.4e}, '
             'content loss: {:.4e}, '
             'time: {:.4f}s'.format(loss, style_score, content_score, time.time() - start_time))
@@ -246,10 +246,10 @@ def run_style_transfer(content_path,
       plt.imshow(img)
       plt.xticks([])
       plt.yticks([])
-    
-      
+
+
   return best_img, best_loss
 
 with tf.device('GPU:0'):
-    best, best_loss = run_style_transfer(content_path, 
+    best, best_loss = run_style_transfer(content_path,
                                         style_path, num_iterations=1000)
